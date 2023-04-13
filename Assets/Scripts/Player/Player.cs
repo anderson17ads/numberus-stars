9fcede368e8e9f17da56dbdf321968e96a373d77
Anderson de Souza <anderson17ads@hotmail.com>
2023-04-13 00:33:45 -0300

    Aplicando mecânica de movimentação de mouse e touch

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,38 +3,57 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float speed;
+    private float speedKeyboard;
+
+    [SerializeField]
+    private float speedMouse;
 
     private Rigidbody2D rig;
-
-    private Vector2 inputDirection;
 
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
-    {
-        onInput();
-    }
-
     private void FixedUpdate() 
     {
-        onMove();
+        onMoveKeyboard();
+        onMoveMouse();
+        onMoveTouch();
     }
 
-    private void onInput()
+    private void onMoveKeyboard()
     {
-        inputDirection = new Vector2(
-            Input.GetAxisRaw("Horizontal"), 
-            Input.GetAxisRaw("Vertical")
-        );
+        if ((Input.GetAxis("Horizontal") != 0) || (Input.GetAxis("Vertical") != 0)) {
+            Vector2 inputDirection = new Vector2(
+                Input.GetAxisRaw("Horizontal"), 
+                Input.GetAxisRaw("Vertical")
+            );
+
+            rig.MovePosition(rig.position + inputDirection * speedKeyboard * Time.fixedDeltaTime);
+        }
     }
 
-    private void onMove()
+    private void onMoveMouse()
     {
-        rig.MovePosition(rig.position + inputDirection * speed * Time.fixedDeltaTime);
+        if ((Input.GetAxis("Mouse X") != 0) || (Input.GetAxis("Mouse Y") != 0)) {
+            rig.position = Vector2.Lerp(
+                this.transform.position,
+                Camera.main.ScreenToWorldPoint(Input.mousePosition),
+                (speedMouse * Time.fixedDeltaTime)
+            );
+        }
+    }
+
+    private void onMoveTouch()
+    {
+        if (Input.touchCount > 0) {
+            rig.position = Vector2.Lerp(
+                this.transform.position,
+                Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position),
+                (speedMouse * Time.fixedDeltaTime)
+            );
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision) 
